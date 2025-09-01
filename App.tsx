87ba88c4ea105096afa6e490475cb8c841990c15
Alexs1313 <aleshaalesha1313@gmail.com>
2025-09-01 13:34:39 +0300

    upd

--- App.tsx
+++ App.tsx
@@ -29,7 +29,7 @@ const App = () => {
           <Stack.Navigator screenOptions={{ headerShown: false }}>
             <Stack.Screen
               name="WelcomeWildTimeScreen"
-              component={WelcomeWildTimeScreen}
+              component={WelcomeWildTimeScree}
             />
             <Stack.Screen
               name="WildTimeHomeScreen"
